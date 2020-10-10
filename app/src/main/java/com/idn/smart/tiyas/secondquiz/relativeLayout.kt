b4b39fb9c535b54d1df0bc3da33ca84ed73de89c
TiyasAria <tiyasaria123@gmail.com>
2020-10-10 15:14:38 +0700

    Initial commit

---
+++ app/src/main/java/com/idn/smart/tiyas/secondquiz/relativeLayout.kt
@@ -0,0 +1,11 @@
+package com.idn.smart.tiyas.secondquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class relativeLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout)
+    }
+}
